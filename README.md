8f1986a634eeb52f2768e0447a85959010d1a7b5
mahathi0387 <mahathi.obbineni@gmail.com>
2019-09-13 08:18:55 +0530

    test

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # devops
 training
+change
---
+++ increment.ps1
@@ -0,0 +1,16 @@
+﻿[xml]$xmldata = Get-Content "C:\Users\HAI\maventest\Maventestapp\pom.xml"
+
+
+
+
+$Version = $xmldata.project.version
+$VersionSplitHyphen = $Version -split '-'
+write-host "version is" $VersionSplitHyphen[0]
+
+
+#$fileContents  = ($VersionSplitHyphen[0]| Select -Last 1)
+#$parts=$fileContents.Split('=')
+#$versionString = $parts[1].Replace('"','"')
+#$version = [Version]$versionString
+#$newVersionString = (New-Object -TypeName 'System.Version' -ArgumentList @($version.Major, ($version.Minor+1), $version.Build)).ToString()
+#(gc $Verfile) -replace "$versionString", "$newVersionString" | sc $Verfile
